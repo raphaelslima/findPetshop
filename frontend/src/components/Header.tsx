79d60e72792a1069b04fcce5c4fd2ac0cd043ece
Raphael Lima <raphaeldesousalm@gmal.com>
2023-10-19 18:10:23 -0300

    feat: change logo and responsive header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,16 +1,12 @@
 import Image from 'next/image';
 
 import logoDIT from '../../public/logoDTI.png';
-import Button from './Button';
 
 const Header = () => {
   return (
-    <header className="container-lg h-20 flex items-center justify-between p-4">
+    <header className="container-lg h-20 flex items-center justify-start p-4">
       <div>
         <Image src={logoDIT} alt="Logo DTI" width={48} height={48} />
-      </div>
-      <div>
-        <Button>Fazer login</Button>
       </div>
     </header>
   );
